f7be662ea1fe0b36dd193e118403176c76dcfd6e
Deomin Dmitriy <58627@bk.ru>
2024-07-09 19:06:07 +0300

    -

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,7 @@ const BACKSPACE: char = 8u8 as char;
 const FILE_CONFIG: &str = "confSeedGenerator.txt";
 
 
-fn main(){
-    let version: &str = env!("CARGO_PKG_VERSION");
-    println!("{}", blue("==================="));
-    println!("{}{}", blue("FIND SEED GENERATOR v:"), magenta(version));
-    println!("{}", blue("==================="));
-
+fn main() {
     //Чтение настроек, и если их нет создадим
     //-----------------------------------------------------------------
     let conf = match lines_from_file(&FILE_CONFIG) {
@@ -39,6 +34,7 @@ fn main(){
     let comb_perebor_left_: usize = first_word(&conf[3].to_string()).to_string().parse::<usize>().unwrap();
     let rand_alfabet = string_to_bool(first_word(&conf[4].to_string()).to_string());
     let size_rand_alfabet = first_word(&conf[5].to_string()).to_string().parse::<usize>().unwrap();
+    let vivod = string_to_bool(first_word(&conf[6].to_string()).to_string());
     //---------------------------------------------------------------------
 
     //если укажут меньше или 0
@@ -50,30 +46,36 @@ fn main(){
     let mut rng = rand::thread_rng();
 
 
-    println!("{}{}", blue("ДЛИНА ФРАЗЫ:"), green(dlinn_a_pasvord));
-
     //-------------------------------------------------------------------------
     // Преобразуем строки в вектор
     let mut lines = WORDS.iter().map(|&s| s.to_string()).collect();
     if rand_alfabet { lines = get_rand_list(lines, size_rand_alfabet) };
-    if rand_alfabet {
-       println!("{}{}", blue("СЛУЧАЙНЫЕ ИЗ СПИСКА:"), green("ВКЛЮЧЕННО"));
-        println!("{}{}", blue("-КОЛИЧЕСТВО СЛУЧАЙНЫХ ИЗ СПИСКА:"), green(size_rand_alfabet));
-        println!("{}{}", blue("-СПИСОК:"), green(lines.join(" ")));
-    };
-    println!("{}{}", blue("КОЛИЧЕСТВО СЛОВ ПЕРЕБОРА СЛЕВА:"), green(comb_perebor_left));
-    println!("{}{}", blue("НАЧАЛО ПЕРЕБОРА:"), green(start_perebor.clone()));
+    if vivod {
+        let version: &str = env!("CARGO_PKG_VERSION");
+        println!("{}", blue("==================="));
+        println!("{}{}", blue("FIND SEED GENERATOR v:"), magenta(version));
+        println!("{}", blue("==================="));
 
-    //-------------------------------------------------------------------------------
-    if mode > 2 {
-        println!("{}", red("!!!"));
-        println!("{}", red(format!("{mode} ТАКОГО РЕЖИМА РАБОТА ПОКА НЕТ\nесть:\n0 последовательный перебор\n1 рандом\n2 комбинированый")));
-        println!("{}", red("!!!"));
-        jdem_user_to_close_programm();
+        println!("{}{}", blue("ДЛИНА ФРАЗЫ:"), green(dlinn_a_pasvord));
+        if rand_alfabet {
+            println!("{}{}", blue("СЛУЧАЙНЫЕ ИЗ СПИСКА:"), green("ВКЛЮЧЕННО"));
+            println!("{}{}", blue("-КОЛИЧЕСТВО СЛУЧАЙНЫХ ИЗ СПИСКА:"), green(size_rand_alfabet));
+            println!("{}{}", blue("-СПИСОК:"), green(lines.join(" ")));
+        };
+
+        println!("{}{}", blue("КОЛИЧЕСТВО СЛОВ ПЕРЕБОРА СЛЕВА:"), green(comb_perebor_left));
+        println!("{}{}", blue("НАЧАЛО ПЕРЕБОРА:"), green(start_perebor.clone()));
+        //-------------------------------------------------------------------------------
+        if mode > 2 {
+            println!("{}", red("!!!"));
+            println!("{}", red(format!("{mode} ТАКОГО РЕЖИМА РАБОТА ПОКА НЕТ\nесть:\n0 последовательный перебор\n1 рандом\n2 комбинированый")));
+            println!("{}", red("!!!"));
+            jdem_user_to_close_programm();
+        }
+        println!("{}{}", blue("РЕЖИМ ГЕНЕРАЦИИ ПАРОЛЯ:"), green(get_mode_text(mode)));
+
+        println!("{}", blue("************************************"));
     }
-    println!("{}{}", blue("РЕЖИМ ГЕНЕРАЦИИ ПАРОЛЯ:"), green(get_mode_text(mode)));
-
-    println!("{}", blue("************************************"));
 
 
     //для измерения скорости
@@ -92,7 +94,7 @@ fn main(){
             Some(&ch) => {
                 // Находим позицию слова в charset_chars
                 lines.iter().position(|c| c == ch).unwrap_or_else(|| {
-                    eprintln!("{}", format!("Слово:{} из *начала перебора* не найдено, установлено первое из словаря", ch));
+                    if vivod { eprintln!("{}", format!("Слово:{} из *начала перебора* не найдено, установлено первое из словаря", ch)); }
                     0
                 })
             }
@@ -101,14 +103,45 @@ fn main(){
         current_combination[d] = position;
     }
 
+
     //--ГЛАВНЫЙ ЦИКЛ
     // слушаем ответы потоков и если есть шлём новую задачу
     let mut password_string = "инициализация".to_string();
+    let mut s = String::new();
+    for i in current_combination.iter() {
+        s.push_str(lines.get(*i).unwrap());
+        s.push(' ');
+    }
 
-    let mut info  =0;
+    password_string = s.trim().to_string();
+
+    let mut info = 0;
 
     //----------------------------------------------------------------------------------------------
     loop {
+
+        if info>10000{
+            let mut stdout = stdout();
+            print!("{}\r{}", BACKSPACE, green(format!("{total} {password_string} + все последнее" )));
+            stdout.flush().unwrap();
+            info=0;
+        }
+
+        //получаем все возможные
+        for i in 0..2048 {
+            let mut mnemonic_test = String::from(format!("{password_string} "));
+            let word_end = data::WORDS[i as usize].to_string();
+            mnemonic_test.push_str(&word_end);
+            if Mnemonic::validate(&mnemonic_test, Language::English).is_ok() {
+                total = total + 1;
+                if vivod {
+                    info = info+1;
+                    add_v_file("mnemonic_list.txt", format!("{}\n", mnemonic_test));
+                } else {
+                    println!("{}", mnemonic_test);
+                }
+            }
+        }
 
         // последовательный перебор
         if mode == 0 {
@@ -124,8 +157,9 @@ fn main(){
             }
 
             if i == 0 && current_combination[0] == charset_len - 1 {
-               println!("{}", blue("ГОТОВО,перебраты все возможные"));
+                println!("{}", blue("ГОТОВО,перебраты все возможные"));
                 jdem_user_to_close_programm();
+                break;
             }
 
             let mut s = String::new();
@@ -182,25 +216,6 @@ fn main(){
 
             password_string = s.trim().to_string();
         }
-
-        info = info+1;
-        //получаем все возможные
-        for i in 0..2048 {
-            let mut mnemonic_test = String::from(format!("{password_string} "));
-            let word_end = data::WORDS[i as usize].to_string();
-            mnemonic_test.push_str(&word_end);
-            if Mnemonic::validate(&mnemonic_test, Language::English).is_ok() {
-                total = total + 1;
-                add_v_file("mnemonic_list.txt", format!("{}\n", mnemonic_test));
-            }
-        }
-        if info>100{
-            let mut stdout = stdout();
-            print!("{}\r{}", BACKSPACE, green(format!("{total} {password_string} + все последнее" )));
-            stdout.flush().unwrap();
-            info=0;
-        }
-
     }
 }
 
@@ -231,12 +246,14 @@ fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
 fn first_word(s: &String) -> &str {
     s.trim().split_whitespace().next().unwrap_or("")
 }
+
 fn jdem_user_to_close_programm() {
     // Ожидание ввода пользователя для завершения программы
     println!("{}", blue("Нажмите Enter, чтобы завершить программу..."));
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Ошибка чтения строки");
 }
+
 //составляем случайный список из полного
 fn get_rand_list(mut list: Vec<String>, size_rand_alfabet: usize) -> Vec<String> {
     let mut rng = thread_rng();
